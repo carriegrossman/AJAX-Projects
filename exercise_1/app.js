490f0854eace875223580741b6f3b1997b227b92
Carrie Grossman <carriegrossman@Carries-MacBook-Pro.local>
2020-06-30 15:51:42 -0400

    files updated

--- exercise_1/app.js
+++ exercise_1/app.js
@@ -1,20 +1,37 @@
+const addNames=(response) =>{
+    console.log(JSON.parse(response))
+
+        let names = JSON.parse(response)
+        let doc = document.querySelector("body")
+        let list = document.createElement("ul")
+        doc.append(list)
+        names.forEach(name => {
+            let listItems = document.createElement("li")
+            list.append(listItems)
+            listItems.append(name.name)
+            
+            let details = document.createElement("div")
+            details.append(name.eye_color, name.height, name.mass)
+            listItems.append(details)
+        });
+        
+        // nameDom.innerText = name.name;
+            // doc.append(nameDom)
+}
 const request = new XMLHttpRequest();
 
 request.onreadystatechange =() => {
     if(request.readyState === 4 && request.status ===200){
-        console.log(request.response)
-        console.log(JSON.parse(request.response))
+        addNames(request.response)
 
-        let names = JSON.parse(request.response)
-        let doc = document.querySelector("body")
+        
 
-        names.forEach(name => {
-            let nameDom = document.createElement("div")
-            nameDom.innerHTML = name.name;
-            doc.append(nameDom)
-        });
+
+        
+
+        
+
     }
 };
-//GET method is the same as doing opening a webpage (Http methods)
-request.open("GET", "./starwars_names.json")
+request.open("GET", "./starwars_names.json");
 request.send();
